72fe483c007495c6a0558d8d22a06d6701c316ec
maccam912 <maccam912@gmail.com>
2014-12-19 11:07:49 -0600

    Update tahoe-run.sh

--- tahoe-run.sh
+++ tahoe-run.sh
@@ -6,7 +6,7 @@ if [ ! -f /root/.tahoe/tahoe.cfg ]; then
   wget --no-check-certificate https://raw.githubusercontent.com/maccam912/docker-tahoe/master/tahoe1
   wget --no-check-certificate https://raw.githubusercontent.com/maccam912/docker-tahoe/master/tahoe2
   cat ./tahoe1 >> tahoe.cfg
-  echo "nickname = "$(< /dev/urandom tr -dc A-Za-z | head -c${1:-32};echo;) >> tahoe.cfg
+  echo "nickname = "$(echo "tub.location = "$(curl -s icanhazip.com)":39499,127.0.0.1:39499" | base64 | head -c${1:-32};echo;) >> tahoe.cfg
   echo "tub.location = "$(curl -s icanhazip.com)":39499,127.0.0.1:39499" >> tahoe.cfg
   cat ./tahoe2 >> tahoe.cfg
   rm tahoe1
